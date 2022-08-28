e620744e7d9bc199b93438a033773a1b29e0d359
Ryani <ryanid.@Ryanis-MacBook-Air.local>
2022-08-28 17:37:19 -0400

    Can see response from API call in console

--- where-in-the-world/src/App.js
+++ where-in-the-world/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+
+function getCountries() {
+  axios
+    .get('https://restcountries.com/v3.1/all')
+    .then(
+      (response) => {
+        console.log(response)
+      },
+      (error) => console.error(error)
+    )
+    .catch((error) => console.error(error))
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
+      <button onClick={getCountries}>Click Me</button>
     </div>
   );
 }
