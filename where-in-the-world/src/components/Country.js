25c4261a9c162c6c1dc42c36dac302ee9092e19b
Ryani <ryanid.@Ryanis-MacBook-Air.local>
2022-08-31 21:58:54 -0400

    Replaced partial name endpoint with full name endpoint to remove countries with duplicate names

--- where-in-the-world/src/components/Country.js
+++ where-in-the-world/src/components/Country.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 import axios from 'axios'
 
@@ -10,7 +10,7 @@ function Country() {
 
     useEffect(() => {
         axios
-        .get(`https://restcountries.com/v3.1/name/${name}`)
+        .get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
         .then(
         (response) => {
             console.log(response.data)
@@ -23,25 +23,26 @@ function Country() {
     },[name])
 
     return (
-    <div className='info'>
-         {country.map(c => (
-            <div key={c.name.common}>               
-                <h1>{c.name.common}</h1>
-                <img src={c.flags.png} alt={`Flag of ${c.name.common}`} className='info-flag'/>
-                <p><b>Native Name:</b> {c.altSpellings[1]}</p>
-                <p><b>Population:</b> {c.population.toLocaleString()}</p>
-                <p><b>Region:</b> {c.region}</p>
-                <p><b>Sub Region:</b> {c.subregion}</p>
-                <p><b>Capital:</b> {c.capital[0]}</p>
-                <p><b>Top Level Domain:</b> {c.tld[0]}</p>
-                {country.map(curr => 
-                    <p><b>Currencies:</b>{curr.currencies[name]}</p>
-                )}
-                
-                <p><b>Languages:</b> {c.language}</p>
-            </div>
-         ))}
-    </div> 
+        <div className='info'>
+            <button><Link to='/' style={{ textDecoration: 'none', color: 'black' }}>Back</Link></button>
+            {country.map(c => (
+                <div key={c.name.common}>               
+                    <h1>{c.name.common}</h1>
+                    <img src={c.flags.png} alt={`Flag of ${c.name.common}`} className='info-flag'/>
+                    <p><b>Native Name:</b> {c.altSpellings[1]}</p>
+                    <p><b>Population:</b> {c.population.toLocaleString()}</p>
+                    <p><b>Region:</b> {c.region}</p>
+                    <p><b>Sub Region:</b> {c.subregion}</p>
+                    <p><b>Capital:</b> {c.capital[0]}</p>
+                    <p><b>Top Level Domain:</b> {c.tld[0]}</p>
+                    {country.map(curr => 
+                        <p><b>Currencies:</b>{curr.currencies.name}</p>
+                    )}
+                    
+                    <p><b>Languages:</b> {c.language}</p>
+                </div>
+            ))}
+        </div> 
   )
     }
   
